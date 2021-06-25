00d12ea00247382507fe8a41e8e70c40f19649b9
achu072 <68572833+achu072@users.noreply.github.com>
2021-06-25 13:38:27 +1200

    Implemented external interrupt to toggle LED with push button

--- stm32f3-test/src2/exti.c
+++ stm32f3-test/src2/exti.c
@@ -5,26 +5,25 @@
 void EXTI_init(void) {
     // Enable SYSCFG clock
     RCC->APB2ENR |= (1 << 0);
-    // Connect EXTI line to PE9
-    SYSCFG->EXTICR[3] |= (1 << 6);
-    SYSCFG->EXTICR[3] &= ~((1 << 5) | (1 << 4));
-    // Enable interrupt on line PE9
-    EXTI->IMR |= (1 << 9);
+    // Connect EXTI line to PA0
+    SYSCFG->EXTICR[1] &= ~((1 << 2) | (1 << 1) | (1 << 0));
+    // Enable interrupt on line PA0
+    EXTI->IMR |= (1 << 0);
     // Disable trigger on rising edge
-    EXTI->RTSR &= ~(1 << 9);
+    EXTI->RTSR &= ~(1 << 0);
     // Enable trigger on falling edge
-    EXTI -> FTSR |= (1 << 9);
+    EXTI -> FTSR |= (1 << 0);
     // Set priority for interrupt
-    NVIC_SetPriority(EXTI9_5_IRQn, 0x03);
+    NVIC_SetPriority(EXTI0_IRQn, 0x03);
     // Enable interrupt
-    NVIC_EnableIRQ(EXTI9_5_IRQn);
+    NVIC_EnableIRQ(EXTI0_IRQn);
 }
 
 // Write handler for EXTI_9_5_IRQn interrupt
-void EXTI_5_9_IRQn_handler(void) {
-    if (EXTI->PR & (1 << 9)) {
+void EXTI0_IRQHandler(void) {
+    if (EXTI->PR & (1 << 0)) {
             // Clear interrupt status flag
-            EXTI->PR |= (1 << 9);
-            led_toggle;
+            EXTI->PR |= (1 << 0);
+            led_toggle();
     }
 }
